318b7ecad88d9a80d64dbb105785e1e64748574d
Rodrigo Alceu Baliza <127142342+rodrigo-baliza@users.noreply.github.com>
2024-03-14 11:54:24 -0300

    finished operations

--- feature/tuple.go
+++ feature/tuple.go
@@ -1,10 +1,15 @@
 package feature
 
-import "errors"
+import (
+	"errors"
+	"math"
+)
 
 var (
-	ErrAddTwoPoints = errors.New("can't add two points")
+	ErrAddTwoPoints       = errors.New("can't add two points")
 	ErrSubPointFromVector = errors.New("can't sub point from vector")
+	ErrDivByZero          = errors.New("can't div by zero")
+	ErrNotVector          = errors.New("it's not a vector")
 )
 
 // Tuple is the primitive type for any ray tracing operations.
@@ -45,6 +50,11 @@ func (t Tuple) IsVector() bool {
 	return t.W == 0.0
 }
 
+// IsVector returns if the Tuple is a vector.
+func (t Tuple) IsEqual(o Tuple) bool {
+	return isEqual(t.X, o.X) && isEqual(t.Y, o.Y) && isEqual(t.Z, o.Z) && isEqual(t.W, o.W)
+}
+
 // Add sums two Tuple values and returns a new Tuple.
 // It can return an error if both Tuples are points.
 func (t Tuple) Add(o Tuple) (Tuple, error) {
@@ -77,4 +87,102 @@ func (t Tuple) Sub(o Tuple) (Tuple, error) {
 	r.W = t.W - o.W
 
 	return r, nil
+}
+
+// Neg is the opposite of a tuple. It subtracts the vector from (0, 0, 0, 0).
+func (t Tuple) Neg() Tuple {
+	return Tuple{
+		X: -t.X,
+		Y: -t.Y,
+		Z: -t.Z,
+		W: -t.W,
+	}
+}
+
+// Mul is the multiplication of a tuple by a scalar.
+func (t Tuple) Mul(s float64) Tuple {
+	return Tuple{
+		X: t.X * s,
+		Y: t.Y * s,
+		Z: t.Z * s,
+		W: t.W * s,
+	}
+}
+
+// Div is the division of a tuple by a scalar.
+func (t Tuple) Div(s float64) (Tuple, error) {
+	var r Tuple
+
+	if s == 0.0 {
+		return r, ErrDivByZero
+	}
+
+	r.X = t.X / s
+	r.Y = t.Y / s
+	r.Z = t.Z / s
+	r.W = t.W / s
+
+	return r, nil
+}
+
+// DotProduct is the multiplication of a vector by other vector and returns a scalar.
+func (t Tuple) DotProduct(o Tuple) (float64, error) {
+	var s float64
+
+	if !t.IsVector() || !o.IsVector() {
+		return s, ErrNotVector
+	}
+
+	s = t.X*o.X + t.Y*o.Y + t.Z*o.Z + t.W*o.W
+
+	return s, nil
+}
+
+// CrossProduct is the multiplication of a vector by other vector and returns a vector.
+func (t Tuple) CrossProduct(o Tuple) (Tuple, error) {
+	var c Tuple
+
+	if !t.IsVector() || !o.IsVector() {
+		return c, ErrNotVector
+	}
+
+	c.X = t.Y*o.Z - t.Z*o.Y
+	c.Y = t.Z*o.X - t.X*o.Z
+	c.Z = t.X*o.Y - t.Y*o.X
+
+	return c, nil
+}
+
+// Magnitude is the distance represented by a vector.
+func (t Tuple) Magnitude() (float64, error) {
+	var m float64
+
+	if !t.IsVector() {
+		return m, ErrNotVector
+	}
+
+	m = math.Sqrt(math.Pow(t.X, 2.0) + math.Pow(t.Y, 2.0) + math.Pow(t.Z, 2.0) + math.Pow(t.W, 2.0))
+
+	return m, nil
+}
+
+// Normalize is the normalization of a vector.
+func (t Tuple) Normalize() (Tuple, error) {
+	var n Tuple
+
+	if !t.IsVector() {
+		return n, ErrNotVector
+	}
+
+	mag, err := t.Magnitude()
+	if err != nil {
+		return n, err
+	}
+
+	n.X = t.X / mag
+	n.Y = t.Y / mag
+	n.Z = t.Z / mag
+	n.W = t.W / mag
+
+	return n, nil
 }
--- feature/tuple_test.go
+++ feature/tuple_test.go
@@ -2,6 +2,7 @@ package feature_test
 
 import (
 	"errors"
+	"math"
 	"ray-tracer/feature"
 	"strings"
 	"testing"
@@ -162,8 +163,8 @@ func TestAdd(t *testing.T) {
 			if !errors.Is(err, test.err) {
 				t.Errorf("%q: got error %v, expected error %v", test.name, err, test.err)
 			}
-			if diff := pretty.Diff(got, test.want); len(diff) != 0 {
-				t.Errorf("\n%s", strings.Join(diff, "\n"))
+			if !test.want.IsEqual(got) {
+				t.Errorf("%s wants %+v and got %+v", test.name, test.want, got)
 			}
 		})
 	}
@@ -268,8 +269,523 @@ func TestSub(t *testing.T) {
 			if !errors.Is(err, test.err) {
 				t.Errorf("%q: got error %v, expected error %v", test.name, err, test.err)
 			}
-			if diff := pretty.Diff(got, test.want); len(diff) != 0 {
-				t.Errorf("\n%s", strings.Join(diff, "\n"))
+			if !test.want.IsEqual(got) {
+				t.Errorf("%s wants %+v and got %+v", test.name, test.want, got)
+			}
+		})
+	}
+}
+
+func TestNeg(t *testing.T) {
+	tests := []struct {
+		name  string
+		tuple feature.Tuple
+		want  feature.Tuple
+	}{
+		{
+			name: "tuple",
+			tuple: feature.Tuple{
+				X: 1.0,
+				Y: -2.0,
+				Z: 3.0,
+				W: -4.0,
+			},
+			want: feature.Tuple{
+				X: -1.0,
+				Y: 2.0,
+				Z: -3.0,
+				W: 4.0,
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := test.tuple.Neg()
+
+			if !test.want.IsEqual(got) {
+				t.Errorf("%s wants %+v and got %+v", test.name, test.want, got)
+			}
+		})
+	}
+}
+
+func TestMul(t *testing.T) {
+	tests := []struct {
+		name   string
+		tuple  feature.Tuple
+		scalar float64
+		want   feature.Tuple
+	}{
+		{
+			name: "gt one",
+			tuple: feature.Tuple{
+				X: 1.0,
+				Y: -2.0,
+				Z: 3.0,
+				W: -4.0,
+			},
+			scalar: 3.5,
+			want: feature.Tuple{
+				X: 3.5,
+				Y: -7.0,
+				Z: 10.5,
+				W: -14.0,
+			},
+		},
+		{
+			name: "lt one",
+			tuple: feature.Tuple{
+				X: 1.0,
+				Y: -2.0,
+				Z: 3.0,
+				W: -4.0,
+			},
+			scalar: 0.5,
+			want: feature.Tuple{
+				X: 0.5,
+				Y: -1.0,
+				Z: 1.5,
+				W: -2.0,
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := test.tuple.Mul(test.scalar)
+
+			if !test.want.IsEqual(got) {
+				t.Errorf("%s wants %+v and got %+v", test.name, test.want, got)
+			}
+		})
+	}
+}
+
+func TestDiv(t *testing.T) {
+	tests := []struct {
+		name   string
+		tuple  feature.Tuple
+		scalar float64
+		want   feature.Tuple
+		err    error
+	}{
+		{
+			name: "gt one",
+			tuple: feature.Tuple{
+				X: 1.0,
+				Y: -2.0,
+				Z: 3.0,
+				W: -4.0,
+			},
+			scalar: 2.0,
+			want: feature.Tuple{
+				X: 0.5,
+				Y: -1.0,
+				Z: 1.5,
+				W: -2.0,
+			},
+			err: nil,
+		},
+		{
+			name: "lt one",
+			tuple: feature.Tuple{
+				X: 1.0,
+				Y: -2.0,
+				Z: 3.0,
+				W: -4.0,
+			},
+			scalar: 0.5,
+			want: feature.Tuple{
+				X: 2.0,
+				Y: -4.0,
+				Z: 6.0,
+				W: -8.0,
+			},
+			err: nil,
+		},
+		{
+			name: "by zero",
+			tuple: feature.Tuple{
+				X: 1.0,
+				Y: -2.0,
+				Z: 3.0,
+				W: -4.0,
+			},
+			scalar: 0.0,
+			want:   feature.Tuple{},
+			err:    feature.ErrDivByZero,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := test.tuple.Div(test.scalar)
+
+			if !errors.Is(err, test.err) {
+				t.Errorf("%q: got error %v, expected error %v", test.name, err, test.err)
+			}
+			if !test.want.IsEqual(got) {
+				t.Errorf("%s wants %+v and got %+v", test.name, test.want, got)
+			}
+		})
+	}
+}
+
+func TestMagnitude(t *testing.T) {
+	tests := []struct {
+		name  string
+		tuple feature.Tuple
+		want  float64
+		err   error
+	}{
+		{
+			name: "x 1",
+			tuple: feature.Tuple{
+				X: 1.0,
+				Y: 0.0,
+				Z: 0.0,
+				W: 0.0,
+			},
+			want: 1.0,
+			err:  nil,
+		},
+		{
+			name: "y 1",
+			tuple: feature.Tuple{
+				X: 0.0,
+				Y: 1.0,
+				Z: 0.0,
+				W: 0.0,
+			},
+			want: 1.0,
+			err:  nil,
+		},
+		{
+			name: "z 1",
+			tuple: feature.Tuple{
+				X: 0.0,
+				Y: 0.0,
+				Z: 1.0,
+				W: 0.0,
+			},
+			want: 1.0,
+			err:  nil,
+		},
+		{
+			name: "positive vector",
+			tuple: feature.Tuple{
+				X: 1.0,
+				Y: 2.0,
+				Z: 3.0,
+				W: 0.0,
+			},
+			want: math.Sqrt(14.0),
+			err:  nil,
+		},
+		{
+			name: "negative vector",
+			tuple: feature.Tuple{
+				X: -1.0,
+				Y: -2.0,
+				Z: -3.0,
+				W: 0.0,
+			},
+			want: math.Sqrt(14.0),
+			err:  nil,
+		},
+		{
+			name: "not a vector",
+			tuple: feature.Tuple{
+				X: 1.0,
+				Y: 2.0,
+				Z: 3.0,
+				W: 1.0,
+			},
+			want: 0.0,
+			err:  feature.ErrNotVector,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := test.tuple.Magnitude()
+
+			if !errors.Is(err, test.err) {
+				t.Errorf("%q: got error %v, expected error %v", test.name, err, test.err)
+			}
+			if test.want != got {
+				t.Errorf("%s wants %f and got %f", test.name, test.want, got)
+			}
+		})
+	}
+}
+
+func TestNormalize(t *testing.T) {
+	tests := []struct {
+		name  string
+		tuple feature.Tuple
+		want  feature.Tuple
+		err   error
+	}{
+		{
+			name: "case 1",
+			tuple: feature.Tuple{
+				X: 4.0,
+				Y: 0.0,
+				Z: 0.0,
+				W: 0.0,
+			},
+			want: feature.Tuple{
+				X: 1.0,
+				Y: 0.0,
+				Z: 0.0,
+				W: 0.0,
+			},
+			err: nil,
+		},
+		{
+			name: "case 2",
+			tuple: feature.Tuple{
+				X: 1.0,
+				Y: 2.0,
+				Z: 3.0,
+				W: 0.0,
+			},
+			want: feature.Tuple{
+				X: 0.26726,
+				Y: 0.53452,
+				Z: 0.80178,
+				W: 0.0,
+			},
+			err: nil,
+		},
+		{
+			name: "not a vector",
+			tuple: feature.Tuple{
+				X: 1.0,
+				Y: 2.0,
+				Z: 3.0,
+				W: 1.0,
+			},
+			want: feature.Tuple{},
+			err:  feature.ErrNotVector,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := test.tuple.Normalize()
+
+			if !errors.Is(err, test.err) {
+				t.Errorf("%q: got error %v, expected error %v", test.name, err, test.err)
+			}
+			if !test.want.IsEqual(got) {
+				t.Errorf("%s wants %+v and got %+v", test.name, test.want, got)
+			}
+		})
+	}
+}
+
+func TestDotProduct(t *testing.T) {
+	tests := []struct {
+		name   string
+		first  feature.Tuple
+		second feature.Tuple
+		want   float64
+		err    error
+	}{
+		{
+			name: "case 1",
+			first: feature.Tuple{
+				X: 1.0,
+				Y: 2.0,
+				Z: 3.0,
+				W: 0.0,
+			},
+			second: feature.Tuple{
+				X: 2.0,
+				Y: 3.0,
+				Z: 4.0,
+				W: 0.0,
+			},
+			want: 20.0,
+			err:  nil,
+		},
+		{
+			name: "case 2",
+			first: feature.Tuple{
+				X: 1.0,
+				Y: 2.0,
+				Z: 3.0,
+				W: 0.0,
+			},
+			second: feature.Tuple{
+				X: -2.0,
+				Y: -3.0,
+				Z: -4.0,
+				W: 0.0,
+			},
+			want: -20.0,
+			err:  nil,
+		},
+		{
+			name: "case 3",
+			first: feature.Tuple{
+				X: 1.0,
+				Y: 2.0,
+				Z: 3.0,
+				W: 0.0,
+			},
+			second: feature.Tuple{
+				X: 2.0,
+				Y: 3.0,
+				Z: -4.0,
+				W: 0.0,
+			},
+			want: -4.0,
+			err:  nil,
+		},
+		{
+			name: "invalid 1",
+			first: feature.Tuple{
+				X: 1.0,
+				Y: 2.0,
+				Z: 3.0,
+				W: 1.0,
+			},
+			second: feature.Tuple{
+				X: 2.0,
+				Y: 3.0,
+				Z: 4.0,
+				W: 0.0,
+			},
+			want: 0.0,
+			err:  feature.ErrNotVector,
+		},
+		{
+			name: "invalid 2",
+			first: feature.Tuple{
+				X: 1.0,
+				Y: 2.0,
+				Z: 3.0,
+				W: 0.0,
+			},
+			second: feature.Tuple{
+				X: 2.0,
+				Y: 3.0,
+				Z: 4.0,
+				W: 1.0,
+			},
+			want: 0.0,
+			err:  feature.ErrNotVector,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := test.first.DotProduct(test.second)
+
+			if !errors.Is(err, test.err) {
+				t.Errorf("%q: got error %v, expected error %v", test.name, err, test.err)
+			}
+			if test.want != got {
+				t.Errorf("%s wants %f and got %f", test.name, test.want, got)
+			}
+		})
+	}
+}
+
+func TestCrossProduct(t *testing.T) {
+	tests := []struct {
+		name   string
+		first  feature.Tuple
+		second feature.Tuple
+		want   feature.Tuple
+		err    error
+	}{
+		{
+			name: "case 1",
+			first: feature.Tuple{
+				X: 1.0,
+				Y: 2.0,
+				Z: 3.0,
+				W: 0.0,
+			},
+			second: feature.Tuple{
+				X: 2.0,
+				Y: 3.0,
+				Z: 4.0,
+				W: 0.0,
+			},
+			want: feature.Tuple{
+				X: -1.0,
+				Y: 2.0,
+				Z: -1.0,
+				W: 0.0,
+			},
+			err:  nil,
+		},
+		{
+			name: "case 2",
+			first: feature.Tuple{
+				X: 2.0,
+				Y: 3.0,
+				Z: 4.0,
+				W: 0.0,
+			},
+			second: feature.Tuple{
+				X: 1.0,
+				Y: 2.0,
+				Z: 3.0,
+				W: 0.0,
+			},
+			want: feature.Tuple{
+				X: 1.0,
+				Y: -2.0,
+				Z: 1.0,
+				W: 0.0,
+			},
+			err:  nil,
+		},
+		{
+			name: "invalid 1",
+			first: feature.Tuple{
+				X: 2.0,
+				Y: 3.0,
+				Z: 4.0,
+				W: 1.0,
+			},
+			second: feature.Tuple{
+				X: 1.0,
+				Y: 2.0,
+				Z: 3.0,
+				W: 0.0,
+			},
+			want: feature.Tuple{},
+			err:  feature.ErrNotVector,
+		},
+		{
+			name: "invalid 2",
+			first: feature.Tuple{
+				X: 2.0,
+				Y: 3.0,
+				Z: 4.0,
+				W: 0.0,
+			},
+			second: feature.Tuple{
+				X: 1.0,
+				Y: 2.0,
+				Z: 3.0,
+				W: 1.0,
+			},
+			want: feature.Tuple{},
+			err:  feature.ErrNotVector,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := test.first.CrossProduct(test.second)
+
+			if !errors.Is(err, test.err) {
+				t.Errorf("%q: got error %v, expected error %v", test.name, err, test.err)
+			}
+			if !test.want.IsEqual(got) {
+				t.Errorf("%s wants %+v and got %+v", test.name, test.want, got)
 			}
 		})
 	}
